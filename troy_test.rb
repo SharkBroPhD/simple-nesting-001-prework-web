e6f263449aba954b2167f1174d5743f6e01cb54f
SharkBroPhD <troychryssos@gmail.com>
2016-07-11 15:59:29 -0400

    Updated methods to change hashes.

---
+++ troy_test.rb
@@ -0,0 +1,32 @@
+
+def adding_matz
+# add the following information to the top level of the programmer hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP, C"]
+# }
+  programmer_hash = 
+    {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  programmer_hash[:yukihiro_matsumoto]={
+    known_for: "Ruby",
+    languages: ["LISP, C"],}
+
+
+  puts programmer_hash[:yukihiro_matsumoto]
+    
+end
+
+adding_matz
